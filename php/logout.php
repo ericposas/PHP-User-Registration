b2302f566f4aeb961a1c4a69306bd51485b48746
Eric Posas <ericposas06@gmail.com>
2018-08-08 16:54:58 -0500

    css updates

--- php/logout.php
+++ php/logout.php
@@ -9,6 +9,15 @@ unset($_SESSION['photo']);
 unset($_SESSION['id']);
 unset($_SESSION['user_info']);
 
+echo "
+  <script src='https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'></script>
+    <script>WebFont.load({ google: {families: ['Open Sans']} });</script>
+  <style>
+    html{
+      font-family: 'Open Sans', sans-serif;
+    }
+  </style>
+";
 echo "Logging you out...";
 redirect_to("../index.php");
 
